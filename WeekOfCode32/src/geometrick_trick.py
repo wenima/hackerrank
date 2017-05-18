7bce896cf79e09056141ed8376dd8aede4d614f5
MarcMBPDev <marc.wenicker@gmail.com>
2017-05-18 16:59:16 -0700

    inital commit for base test cases

---
+++ WeekOfCode32/src/geometrick_trick.py
@@ -0,0 +1,6 @@
+"""Tests for https://www.hackerrank.com/contests/w32/challenges/geometric-trick."""
+
+def geometricTrick(*args):
+    """Return the number of possible, unique triples of i, j k within a given string
+    made up of a set (a, b, c) following the restraints outlined in the above link."""
+    pass
---
+++ WeekOfCode32/src/test_geometric_trick.py
@@ -0,0 +1,14 @@
+"""Tests for https://www.hackerrank.com/contests/w32/challenges/duplication."""
+
+import pytest
+
+
+TEST = [
+    ('ccaccbbbaccccca', '15', 2),
+]
+
+@pytest.mark.parametrize("s, l, result", TEST)
+def test_build_binary_string(s, l, result):
+    """Test that the correct number of sets is returned."""
+    from geometrick_trick import geometricTrick
+    assert geometricTrick(s, l) == result
