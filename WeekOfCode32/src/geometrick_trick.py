d03e879c48847a648b14707d1347b62a4a2bed13
MarcMBPDev <marc.wenicker@gmail.com>
2017-05-18 19:42:51 -0700

    eliminate loop for k, just do a lookup for remaining divider

--- WeekOfCode32/src/geometrick_trick.py
+++ WeekOfCode32/src/geometrick_trick.py
@@ -22,8 +22,9 @@ def geometricTrick(*args):
             #could be more efficient by checking which has the lowest len, then 2nd lowest; also early exit?
     for j in idx_j:
         for i in idx_i:
-            for k in idx_k:
-                if (j + 1) ** 2 == (i + 1) * (k + 1):
-                    if (i, j, k) in triples: continue
-                    triples.append((i, j, k))
+            if i * 2 + 1 > j ** 2: break
+            k = (j + 1) ** 2 / (i + 1)
+            if k.is_integer():
+                if k - 1 in idx_k and (i, j, k - 1) not in triples:
+                    triples.append((i, j, int(k - 1)))
     return len(triples)
